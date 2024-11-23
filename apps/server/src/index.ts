b7dbd0017eb69a603eef55562728b0d9bed57e33
nxsaloj <niisaloj@gmail.com>
2024-11-23 02:12:36 -0600

    feat(db): adding single product request on server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -14,6 +14,24 @@ app.get('/', (_, res) => {
   return res.json({ ok: true })
 })
 
+app.get('/product/:id', async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const product = await client.product.findUnique({
+      where: { id: Number(id) },
+    })
+    if (product) {
+      return res.json(product)
+    } else {
+      return res.status(404).json({ error: 'Product not found' })
+    }
+  } catch (error) {
+    return res.status(500).json({ error: 'Failed to fetch product' })
+  }
+})
+
+
 const port = process.env.PORT || 5001
 
 app.listen(port, () => {
